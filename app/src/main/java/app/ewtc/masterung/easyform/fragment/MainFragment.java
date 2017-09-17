ec381157a2ebb8610db220a4f1f6ca62460565f0
MasterUNG <phrombutr@gmail.com>
2017-09-17 10:51:46 +0700

    Add Fragment OK

---
+++ app/src/main/java/app/ewtc/masterung/easyform/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package app.ewtc.masterung.easyform.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.ewtc.masterung.easyform.R;
+
+/**
+ * Created by masterung on 9/17/2017 AD.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}   // Main Class
